55a729e9e1f3c719d4cf4f1ff225e79552f9a951
pallavkabra <pallavkabra1@gmail.com>
2018-06-14 09:18:09 +0530

    First commit

---
+++ SpringBootGradle/src/main/java/Main.java
@@ -0,0 +1,21 @@
+class Ideone2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("HELLO");
+	}
+}
+class Ideone1{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("HI");
+	}
+}
+class Ideone3{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hey");
+	}
+}
+
+
